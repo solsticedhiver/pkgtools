eedbe967ba1bff7fa6648e9734d586e16ca6db7c
Daenyth <Daenyth+github@gmail.com>
2009-04-08 15:20:23 -0400

    schema.sql: INTEGER -> REAL
    Signed-off-by: Daenyth <Daenyth+github@gmail.com>

--- other/schema.sql
+++ other/schema.sql
@@ -1,12 +1,12 @@
 CREATE TABLE repos (reponame TEXT, PRIMARY KEY (reponame));
 CREATE TABLE packages (packagename TEXT, PRIMARY KEY (packagename));
 CREATE TABLE versioned_packages (
-	reponame TEXT, packagename TEXT, version TEXT, release INTEGER,
+	reponame TEXT, packagename TEXT, version TEXT, release REAL,
 	PRIMARY KEY (reponame, packagename));
 
 CREATE TABLE files (
 	reponame TEXT, packagename TEXT, version TEXT,
-	release INTEGER, filename TEXT,
+	release REAL, filename TEXT,
 	FOREIGN KEY (packagename) REFERENCES packages,
 	FOREIGN KEY (reponame) REFERENCES repos,
 	PRIMARY KEY (reponame, packagename, filename));
