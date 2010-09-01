9918705bc2aef4b5fdf377cb52249cfe175bb063
solsTiCe d'Hiver <solstice.dhiver@gmail.com>
2010-09-02 00:13:08 +0200

    Rewrite pkgfile in python
    The purpose of this conversion is to use a database file more efficient
    than the database based on a tree of files, to provide faster access
    times and a better user experience.
    
    For example, "time pkgfile -l xscreensaver"
    gives for the bash version
    real	0m28.280s
    user	0m0.597s
    sys	0m0.647s
    
    whereas the new one gives:
    real	0m2.391s
    user	0m0.220s
    sys	0m0.173s
    
    with an empty OS filesystem cache each time
    Cache dropped with:
    echo 3 > /proc/sys/vm/drop_caches
    echo 0 > /proc/sys/vm/drop_caches
    
    About sqlite choice:
    At first, I wanted to use bsddb but bsddb module will be dropped in python
    3.x. So it will need pybsddb, so this makes another dependancy.
    hamsterdb is another possibility; it's GPL.
    But sqlite3 is in the standard library of python, so I go for it.
    
    The downloaded .files.tar.gz are converted to a sqlite file when
    updating the file lists.
    During the update, the local pacman database is also converted to a sqlite
    db file:
    	- to speed up access to pacman db ;-)
    	- to be consistent with other dbs
    	- to avoid using a pacman process
    Note: However pacman is called (at least twice in the code) to get
    pacman db path and to get repo and mirrors
    
    Databases created from files.tar.gz are created from strach every time.
    This could change in the future.
    The local pacman db is only first created from strach (this need at lot
    of I/O) but to avoid that, the next call to -u/--update will just update
    the pkg in local.db file.
    
    With this conversion, using CMD_SEARCH_ENABLED=1 would, at last, be
    something usefull and not painfull to use. You will not have time
    to go and grab some coffee :-P
    
    Also, it is now possible to use --info for sync database, something that
    was not done in the original bash script.
    
    However, use of wget has been dropped to use python standard library
    (urllib.urlretrieve) so RATELIMIT is unsupported. This could be changed
    but simply calling an external process for almost nothing.
    I just spotted bug #20697 http://bugs.archlinux.org/task/20697 that
    noone seems to have noticed so far. So I guess RATELIMIT is not a very
    used option.
    
    The database:
    The format of the sqlite database is simply a row for each package.
    * lists are pickled and zlib compressed (mainly for files list)
    It is posible to avoid these pythonistic things to be able to read those
    dbs from C for example. This will need some adaptation like a simple
    marshalling of lists (of strings); concatenate them for example.
    
    convert_sqlite.py could be used as a standalone script to convert
    .files.tar.gz or a tree like local pacman db into a sqlite file.
    Converting extra.files.tar.gz takes around 40s here with 20MB of memory
    at most.
    
    Original authors will easily go around the pkgfile source code because
    it retains the same structure and functions.
    
    TODO:
    * finish the implementation (search is not done yet)
    * either paste convert_sqlite.py into pkgfile or find a way to
    import it when installed system wide
    * find a better name for convert_sqlite.py
    * polish it and kill bugs
    * name it pkgfile or pkgfile.py ?
    
    I just notice I forgot the -r/--regex action.
    It's almost the same as -g so I do not plan to implement it (yet ?)

---
+++ scripts/alpm2sqlite.py
@@ -0,0 +1,292 @@
+#!/usr/bin/python
+
+###
+# alpm2sqlite.py -- convert an alpm like database to a sqlite one
+# This program is a part of pkgtools
+
+# Copyright (C) 2010 solsTiCe d'Hiver <solstice.dhiver@gmail.com>
+#
+# Pkgtools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# Pkgtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+##
+
+from sys import argv, stderr, exit
+from os import listdir
+from os.path import join, exists, isdir, isfile, basename, dirname
+from tarfile import open as tfopen
+
+from cPickle import loads, dumps
+from zlib import decompress, compress
+import sqlite3
+from datetime import datetime
+
+# _getsection and parse_* functions are borrowed from test/pacman/pmdb.py
+
+def _getsection(fd):
+    i = []
+    while True:
+        line = fd.readline().strip("\n")
+        if not line:
+            break
+        i.append(line)
+    return i
+
+def parse_files(pkg, f):
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip("\n")
+        if line == "%FILES%":
+            pkg['files'] = []
+            while line:
+                line = f.readline().strip("\n")
+                #if line and not line.endswith("/"):
+                # also pick the directories
+                if line:
+                    pkg['files'].append(line)
+        if line == "%BACKUP%":
+            pkg['backup'] = _getsection(f)
+
+def parse_desc(pkg, f):
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip("\n")
+        if line == "%NAME%":
+            pkg['name'] = f.readline().strip("\n")
+        if line == "%FILENAME%":
+            pkg['filename'] = f.readline().strip("\n")
+        elif line == "%VERSION%":
+            pkg['version'] = f.readline().strip("\n")
+        elif line == "%DESC%":
+            pkg['desc'] = f.readline().strip("\n")
+        elif line == "%GROUPS%":
+            pkg['groups'] = _getsection(f)
+        elif line == "%URL%":
+            pkg['url'] = f.readline().strip("\n")
+        elif line == "%LICENSE%":
+            pkg['license'] = _getsection(f)
+        elif line == "%ARCH%":
+            pkg['arch'] = f.readline().strip("\n")
+        elif line == "%BUILDDATE%":
+            try:
+                pkg['builddate'] = datetime.fromtimestamp(int(f.readline().strip("\n")))
+            except ValueError:
+                pkg['builddate'] = None
+        elif line == "%INSTALLDATE%":
+            try:
+                pkg['installdate'] = datetime.fromtimestamp(int(f.readline().strip("\n")))
+            except ValueError:
+                pkg['installdate'] = None
+        elif line == "%PACKAGER%":
+            pkg['packager'] = f.readline().strip("\n")
+        elif line == "%REASON%":
+            pkg['reason'] = int(f.readline().strip("\n"))
+        elif line == "%SIZE%" or line == "%ISIZE%":
+            pkg['isize'] = int(f.readline().strip("\n"))
+        elif line == "%CSIZE%":
+            pkg['csize'] = int(f.readline().strip("\n"))
+        elif line == "%MD5SUM%":
+            pkg['md5sum'] = f.readline().strip("\n")
+        elif line == "%REPLACES%":
+            pkg['replaces'] = _getsection(f)
+        elif line == "%FORCE%":
+            f.readline()
+            pkg['force'] = 1
+
+def parse_depends(pkg, f):
+    while True:
+        line = f.readline()
+        if not line:
+            break
+        line = line.strip("\n")
+        if line == "%DEPENDS%":
+            pkg['depends'] = _getsection(f)
+        elif line == "%OPTDEPENDS%":
+            pkg['optdepends'] = _getsection(f)
+        elif line == "%CONFLICTS%":
+            pkg['conflicts'] = _getsection(f)
+        elif line == "%PROVIDES%":
+            pkg['provides'] = _getsection(f)
+
+parse_fctn = {'files':parse_files, 'depends':parse_depends, 'desc':parse_desc}
+
+# pickle and zlib compress lists automatically (for files mainly)
+def adapt_list(L):
+    # use a buffer to force a BLOB type in sqlite3
+    return buffer(compress(dumps(L)))
+
+def convert_list(s):
+    return loads(decompress(s))
+
+# Register the adapter
+sqlite3.register_adapter(list, adapt_list)
+
+# Register the converter
+sqlite3.register_converter("list", convert_list)
+
+# sqlite3 module include already adapter/converter for datetime.datetime
+# That's why you'll see datetime in the create statement below
+
+def open_db(dbfile):
+    conn = sqlite3.connect(dbfile, detect_types=sqlite3.PARSE_DECLTYPES)
+    conn.text_factory = str
+    conn.row_factory = sqlite3.Row
+    # do not sync to disk. If the OS crashes, the db will be corrupted.
+    conn.execute('pragma synchronous=0')
+
+    # check the integrity of the db
+    row = conn.execute('pragma integrity_check').fetchone()
+    if row[0] != 'ok':
+        print >> stderr, 'Error: the db %s is corrupted' % dbfile
+        exit(2)
+
+    # create the db if it's not already there
+    conn.execute('''create table if not exists pkg(name varchar(64),
+        filename varchar(64), version varchar(32), desc varchar(512),
+        url varchar(256), packager varchar(128), md5sum varchar(32),
+        arch varchar(6),  builddate datetime, installdate datetime, isize integer,
+        csize integer, reason integer, license list, groups list, depends list,
+        optdepends list, conflicts list, provides list, replaces list,
+        files list, backup list, force integer)''')
+
+    cur = conn.cursor()
+
+    return (conn, cur)
+
+def insert_pkg(pkg, cur):
+    cur.execute('select rowid from pkg where name=?', (pkg['name'],))
+    row = cur.fetchone()
+    if row is not None:
+        cur.execute('update pkg set '+','.join('%s=:%s' % (p,p) for p in pkg)+ ' where name=:name', pkg)
+    else:
+        cur.execute('insert into pkg ('+','.join(p for p in pkg)+') values('+ ','.join(':%s' % p for p in pkg)+')', pkg)
+
+def convert_tarball(tf, conn, cur):
+    # Do not try to create a complete pkg object
+    # but instead commit to db as soon as we have parsed a file
+    for ti in tf:
+        if ti.isfile():
+            f = tf.extractfile(ti)
+            fname = basename(ti.name)
+            if fname not in ('desc', 'depends', 'files'):
+                continue
+            pkgname = '-'.join(basename(dirname(ti.name)).split('-')[:-2])
+            pkg = {'name':pkgname}
+            parse_fctn[fname](pkg, f)
+            f.close()
+
+            insert_pkg(pkg, cur)
+            conn.commit()
+
+def convert_dir(path, conn, cur):
+    for pkgdir in sorted(listdir(path)):
+        pkgpath = '/'.join((path, pkgdir))
+        pkg = {}
+
+        for fname in ('desc', 'depends', 'files'):
+            pathfile = join(pkgpath, fname)
+            if exists(pathfile):
+                with open(pathfile) as f:
+                    parse_fctn[fname](pkg, f)
+
+        insert_pkg(pkg, cur)
+        conn.commit()
+
+def update_repo_from_dir(path, dbfile, options):
+    '''update sqlite db from a directory'''
+    conn, cur = open_db(dbfile)
+
+    # look for new or changed packages
+    for pkgdir in sorted(listdir(path)):
+        # get name and version from dir name
+        pkgname = '-'.join(pkgdir.split('-')[:-2])
+        pkgver = '-'.join(pkgdir.split('-')[-2:])
+
+        pkg = cur.execute('select version from pkg where name=?', (pkgname,)).fetchone()
+        update = True
+        if pkg and pkg[0] == pkgver:
+            update = False
+        # If the pkg has changed, update it
+        if update:
+            pkgpath = '/'.join((path, pkgdir))
+            pkg = {}
+
+            # parse files again
+            for fname in ('desc', 'depends', 'files'):
+                pathfile = join(pkgpath, fname)
+                if exists(pathfile):
+                    with open(pathfile) as f:
+                        parse_fctn[fname](pkg, f)
+
+            insert_pkg(pkg, cur)
+            conn.commit()
+
+    # check for removed package
+    rows = cur.execute('select name,version from pkg')
+    pkgs = rows.fetchmany()
+
+    while pkgs:
+        for pkg in pkgs:
+            pkgname, pkgver = pkg
+            # look for the directory in the alpm db
+            d = join(path, '%s-%s' %( pkgname, pkgver))
+            if not isdir(d):
+                if options.verbose:
+                    print ':: removing %s-%s' % (pkgname, pkgver)
+                cur.execute('delete from pkg where name=?', (pkgname,))
+        pkgs = rows.fetchmany()
+
+def convert(path, dbfile):
+    tf = None
+    if isfile(path):
+        tf = tfopen(path)
+
+    try:
+        conn, cur = open_db(dbfile)
+        # clean the db
+        cur.execute('delete from pkg')
+        if tf is not None:
+            convert_tarball(tf, conn, cur)
+        else:
+            convert_dir(path, conn, cur)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return True
+    except sqlite3.OperationalError, e:
+        print >> stderr, 'Error: %s' % e
+        return False
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print '''Usage: %s [directory|somedb.files.tar.gz]
+Convert an alpm directory or a .files.tar.gz file to a sqlite db file''' % argv[0]
+        exit(1)
+
+    path = argv[1]
+    if isdir(path):
+        dbfile = basename('%s.db' % path.rstrip('/'))
+        print ':: converting %s repo into %s' % (path, dbfile)
+    elif isfile(path):
+        if path.endswith('.files.tar.gz'):
+            dbfile = path.replace('.files.tar.gz', '.db')
+            print ':: converting %s file into %s' % (path, dbfile)
+            tf = tfopen(path)
+
+        convert(path, dbfile)
---
+++ scripts/pkgfile.py
@@ -0,0 +1,403 @@
+#!/usr/bin/python
+###
+# pkgfile.py -- search the arch repo to see what package owns a file
+# This program is a part of pkgtools
+
+# Copyright (C) 2008-2010 Daenyth <Daenyth+Arch _AT_ gmail _DOT_ com>
+# Copyright (C) 2010 solsTiCe d'Hiver <solstice.dhiver@gmail.com>
+#
+# Pkgtools is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# Pkgtools is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+##
+
+from sys import exit, stderr
+from os.path import exists, join
+from os import unlink, getpid, listdir, getenv, umask
+from optparse import OptionParser, OptionGroup
+import signal
+from subprocess import Popen, PIPE
+import re
+from urllib import urlretrieve
+from alpm2sqlite import convert, open_db, update_repo_from_dir
+
+server = re.compile(r'.*adding new server URL to database \'(.*)\': (.*)')
+
+VERSION = '22'
+CONFIG_DIR = '/etc/pkgtools'
+FILELIST_DIR = '/usr/share/pkgtools'
+LOCKFILE = '/var/lock/pkgfile'
+
+def find_dbpath():
+    '''find pacman dbpath'''
+    p = Popen(['pacman', '-v'], stdout=PIPE)
+    output = p.communicate()[0]
+    for line in output.split('\n'):
+        if line.startswith('DB Path'):
+            return line.split(':')[1].strip()
+
+def parse_config(filename, comment_char='#', option_char='='):
+    # Borrowed at http://www.decalage.info/en/python/configparser
+    # another option is http://mail.python.org/pipermail/python-dev/2002-November/029987.html
+    options = {}
+    try:
+        f = open(filename)
+        for line in f:
+            line = line.strip()
+            if comment_char in line:
+                line, comment = line.split(comment_char, 1)
+            if option_char in line:
+                option, value = line.split(option_char, 1)
+                option = option.strip()
+                value = value.strip()
+                try:
+                    options[option] = int(value)
+                except ValueError:
+                    options[option] = value
+        f.close()
+    except IOError:
+        pass
+    return options
+
+def load_config(conf_file):
+    options = parse_config(join(CONFIG_DIR, conf_file))
+    XDG_CONFIG_HOME = getenv('XDG_CONFIG_HOME')
+    xdg_conf_file = join(XDG_CONFIG_HOME, 'pkgtools', conf_file)
+    if exists(xdg_conf_file):
+        tmp = parse_config(xdg_conf_file)
+        for k in tmp:
+            options[k] = tmp[k]
+    # NOT IMPLEMENTED: ${HOME}/.pkgtools/pkgfile.conf.
+    # We could say it's depreciated and obsolete
+    return options
+
+def die(n=-1, msg='Unknown error'):
+    print >> stderr, msg
+    exit(n)
+
+def lock():
+    if exists(LOCKFILE):
+        die(1, 'Error: Unable to take lock at %s' % LOCKFILE)
+    try:
+        with open(LOCKFILE, 'w') as f:
+            f.write('%d' % getpid())
+    except IOError:
+        die(1, 'Error: Unable to take lock at %s' % LOCKFILE)
+
+def unlock():
+    try:
+        unlink(LOCKFILE)
+    except OSError:
+        print >> stderr, 'Warning: Failed to unlock %s' % LOCKFILE
+
+def handle_SIGINT(signum, frame):
+    unlock()
+    die(1, 'Caught SIGINT -- aborting!')
+
+def handle_SIGTERM(signum, frame):
+    unlock()
+    die(1, 'Killed!')
+
+PKG_ATTRS = ('name', 'filename', 'version', 'desc', 'groups', 'url', 'license', 'arch',
+        'builddate', 'installdate', 'packager', 'reason', 'isize', 'csize', 'md5sum',
+        'replaces', 'force', 'depends', 'optdepends', 'conflicts',
+        'provides', 'files', 'backup')
+WIDTH = max(len(i) for i in PKG_ATTRS) + 1
+
+def print_pkg(pkg):
+    s = {}
+    for p in PKG_ATTRS:
+        ps = p.capitalize().ljust(WIDTH)
+        try:
+            prop = pkg[p]
+        except KeyError:
+            continue
+        if prop is None:
+            s[p] = '%s: --' % ps
+            continue
+        if p == 'csize' or p == 'isize':
+            s[p] = '%s: %d k' % (ps, prop/1024)
+        #elif p == 'force':
+        #    s[p] = '%s: %d' % (ps, prop)
+        elif p in ('groups', 'license', 'replaces',  'depends', 'optdepends', 'conflicts', 'provides'):
+            s[p] = '%s: %s' % (ps, '  '.join(prop))
+        elif p == 'backup':
+            s[p] = ps+':'
+            for i in prop:
+                s[p] += '\n'+': '.join(i.split('\t')) +'\n'
+            else:
+                s[p] += ' --'
+        #elif p == 'files':
+        #    s[p] = '%s: %s' % (ps, '\n'+'\n'.join(prop))
+        else:
+            s[p] = '%s: %s' % (ps, prop)
+
+    for i in ('name', 'filename', 'version', 'url', 'license', 'groups', 'provides',
+            'depends', 'optdepends', 'conflicts', 'replaces', 'isize','packager',
+            'arch', 'installdate', 'builddate', 'desc'):
+        try:
+            print s[i]
+        except KeyError:
+            pass
+    print
+
+def update(options, target_repo=None):
+    signal.signal(signal.SIGINT, handle_SIGINT)
+    signal.signal(signal.SIGTERM, handle_SIGTERM)
+
+    lock()
+
+    p = Popen(['pacman', '--debug'], stdout=PIPE)
+    output = p.communicate()[0]
+
+    # get a list of repo and mirror
+    res = []
+    for line in output.split('\n'):
+        m = server.match(line)
+        if m:
+            res.append((m.group(1), m.group(2)))
+
+    repo_done = []
+    for repo, mirror in res:
+        if target_repo is not None and repo != target_repo:
+            continue
+        if repo not in repo_done:
+            print ':: Downloading [%s] file list ...' % repo
+            repofile = '%s.files.tar.gz' % repo
+            filelist = join(mirror, repofile)
+
+            try:
+                if options.verbose:
+                    print 'Trying mirror %s ...' % mirror
+                filename, headers = urlretrieve(filelist) # use a temp file
+                print ':: Converting [%s] file list ...' % repo
+                ret = convert(filename, '%s/%s.db' % (FILELIST_DIR, repo))
+                unlink(filename)
+                if not ret:
+                    print >> stderr, 'Warning: Unable to extract %s' % filelist
+                    continue
+
+                repo_done.append(repo)
+                print 'Done'
+            except IOError:
+                print >> stderr, 'Warning: could not retrieve %s' % filelist
+                continue
+
+    print ':: Converting local repo ...'
+    local_db = '%s/local.db' % FILELIST_DIR
+    local_dbpath = join(find_dbpath(), 'local')
+    if exists(local_db):
+        update_repo_from_dir(local_dbpath, local_db, options)
+    else:
+        convert(local_dbpath, local_db)
+    print 'Done'
+
+    unlock()
+
+def list_files(s, options):
+    repo = ''
+    if '/' in s:
+        res = s.split('/')
+        if len(res) > 2:
+            print >> stderr, 'If given foo/bar, assume "bar" package in "foo" repo'
+            return
+        repo, pkg = res
+    else:
+        pkg = s
+
+    sql_stmt, search = ('select name,files from pkg where name=?' , (pkg,))
+    if options.glob:
+        sql_stmt, search = ('select name,files from pkg where name like ?' , (pkg.replace('*', '%').replace('?', '_'),))
+
+    res = []
+    if not options.remote and repo == '':
+        conn, cur = open_db(join(FILELIST_DIR, 'local.db'))
+        rows = cur.execute(sql_stmt, search)
+        matches = rows.fetchmany()
+        while matches:
+            for pkg, files in matches:
+                for f in files :
+                    if options.binaries:
+                        if '/sbin/' in f or '/bin/' in f:
+                            res.append('%s /%s' % (pkg, f))
+                    else:
+                        res.append('%s /%s' % (pkg, f))
+            res.sort()
+            matches = rows.fetchmany()
+
+        cur.close()
+        conn.close()
+
+    if res != []:
+        print '\n'.join(res)
+        return
+
+    repo_list = listdir(FILELIST_DIR)
+    del repo_list[repo_list.index('local.db')]
+    for dbfile in repo_list:
+        if repo != '' and repo != dbfile.replace('.db', ''):
+            continue
+        conn, cur = open_db(join(FILELIST_DIR, dbfile))
+        rows = cur.execute(sql_stmt, search)
+        matches = rows.fetchmany()
+        while matches:
+            for pkg, files in matches:
+                for f in files:
+                    if options.binaries:
+                        if '/sbin/' in f or '/bin/' in f:
+                            res.append('%s /%s' % (pkg, f))
+                    else:
+                        res.append('%s /%s' % (pkg, f))
+            res.sort()
+            matches = rows.fetchmany()
+
+        cur.close()
+        conn.close()
+
+    if res == []:
+        print 'Package "%s" not found' % pkg,
+        if repo != '':
+            print ' in repo %s' % repo,
+        print
+    else:
+        print '\n'.join(res)
+
+def pkgquery(filename, options):
+    if options.glob:
+        from fnmatch import translate
+        regex = translate(filename)
+    else:
+        regex = filename
+    try:
+        filematch = re.compile(regex)
+    except re.error:
+        die(1, 'Error: You need -g option to use * and ?')
+
+    if exists(filename) and not options.remote:
+        conn, cur = open_db('%s/local.db' % FILELIST_DIR)
+        rows = cur.execute('select name,files from pkg')
+
+        pkgfiles = rows.fetchmany()
+        pkgname = ''
+        # search the package name that have a filename
+        while pkgfiles:
+            for n,fls in pkgfiles:
+                for f in fls:
+                    if filematch.match('/'+f):
+                        pkgname = n
+                        break
+            pkgfiles = rows.fetchmany()
+
+        if pkgname == '':
+            cur.close()
+            conn.close()
+            die(1, 'Unable to find package info for file %s' % filename)
+
+        pkg = cur.execute('select * from pkg where name=?', (pkgname,)).fetchone()
+        cur.close()
+        conn.close()
+        print_pkg(pkg)
+        return
+
+    # try in the other repo
+    repo_list = listdir(FILELIST_DIR)
+    del repo_list[repo_list.index('local.db')]
+    pkgname = ''
+    for dbfile in repo_list:
+        conn, cur = open_db(join(FILELIST_DIR, dbfile))
+        rows = cur.execute('select name,files from pkg')
+
+        pkgfiles = rows.fetchmany()
+        # search the package name that have a filename
+        while pkgfiles and not pkgname:
+            for n,fls in pkgfiles:
+                for f in fls:
+                    if filematch.match('/'+f):
+                        pkgname = n
+                        break
+                if pkgname: break
+            pkgfiles = rows.fetchmany()
+
+        if pkgname: break
+        # keep the conn alive if we have found a package
+        cur.close()
+        conn.close()
+
+    if pkgname == '':
+        die(1, 'Unable to find package info for file %s' % filename)
+
+    pkg = cur.execute('select * from pkg where name=?', (pkgname,)).fetchone()
+    cur.close()
+    conn.close()
+    print_pkg(pkg)
+
+if __name__ == '__main__':
+    usage = '%prog [ACTIONS] [OPTIONS] filename'
+    parser = OptionParser(usage=usage, version='%%prog %s' % VERSION)
+    # actions
+    actions = OptionGroup(parser, 'ACTIONS')
+    actions.add_option('-i', '--info', dest='info', action='store_true',
+            default=False, help='provides information about the package owning a file')
+    actions.add_option('-l', '--list', dest='list', action='store_true',
+            default=False, help='list files of a given package; similar to "pacman -Ql"')
+    actions.add_option('-s', '--search', dest='search', action='store_true',
+            default=True, help='search which package owns a file')
+    actions.add_option('-u', '--update', dest='update', action='store_true',
+            default=False, help='update to the latest filelist. This requires write permission to %s' % FILELIST_DIR)
+    parser.add_option_group(actions)
+
+    # options
+    parser.add_option('-b', '--binaries', dest='binaries', action='store_true',
+            default=False, help='only show files in a {s}bin/ directory. Works with -s, -l')
+    parser.add_option('-c', '--case-sensitive', dest='case_sensitive', action='store_true',
+            default=False, help='make searches case sensitive')
+    parser.add_option('-g', '--glob', dest='glob', action='store_true',
+            default=False, help='allow the use of * and ? as wildcards')
+    parser.add_option('-R', '--remote', dest='remote', action='store_true',
+            default=False, help='exclude from the search the local pacman repository')
+    parser.add_option('-v', '--verbose', dest='verbose', action='store_true',
+            default=False, help='enable verbose output')
+
+    (options, args) = parser.parse_args()
+
+    # This section is here for backward compatibilty but there is no need for it
+    # TODO: trash this to the bin with load_config and parse_config
+    dict_options = load_config('pkgfile.conf')
+    try:
+        FILELIST_DIR = dict_options['FILELIST_DIR']
+    except KeyError:
+        pass
+    # PKGTOOLS_DIR is meaningless here
+    # CONFIG_DIR is useless
+    # RATELIMIT is not used (not implemented because wget is not used)
+    # CMD_SEARCH_ENABLED is not used here
+    # UPDATE_CRON neither 
+
+    umask(0022) # This will ensure that any files we create are readable by normal users
+
+    if options.update:
+        try:
+            update(options, target_repo=args[0])
+        except IndexError:
+            update(options)
+    elif options.list:
+        try:
+            list_files(args[0], options)
+        except IndexError:
+            die(1, 'Error: No target specified to search for !')
+    elif options.info:
+        try:
+            pkgquery(args[0], options)
+        except IndexError:
+            die(1, 'Error: No target specified to search for !')
+
